ff5476e6d2d539db9d494b2f5362f9c3e34879fb
chavincc <cc6chavin@gmail.com>
2019-11-08 15:20:06 +0700

    something

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React, { useReducer } from 'react';
 
-import './App.css'
+import './App.css';
 import { Input, Select, Button } from 'antd';
 import { submitAPI } from './api';
 const { Option } = Select;
 
 const initFormData = {
-  errorMessage: "",
-  name: "",
-  email: "",
-  phone: "",
-  male: "",
-}
+  errorMessage: '',
+  name: '',
+  email: '',
+  phone: '',
+  male: ''
+};
 
-const handleSubmit = (formData) => {
+const handleSubmit = formData => {
   // submit formData using submitAPI
-}
+  console.log(formData);
+};
 
-const formReducer = (formData = initFormData, action) => formData
+const $ = id => document.getElementById(id);
+
+const formReducer = (formData = initFormData, action) => formData;
 
 export default () => {
-  const [formData, dispatch] = useReducer(formReducer, initFormData)
+  const [formData, dispatch] = useReducer(formReducer, initFormData);
   return (
     <div className="App">
-      <h1 id="error" style={{ color: "red" }}>{formData.errorMessage}</h1>
-      <Input className="input" placeholder="ชื่อ - นามสกุล" />
-      <Input className="input" placeholder="email" />
-      <Input className="input" placeholder="phone" />
-      <Select className="input" defaultValue="male">
+      <h1 id="error" style={{ color: 'red' }}>
+        {formData.errorMessage}
+      </h1>
+      <Input className="input" id="form-name" placeholder="ชื่อ - นามสกุล" />
+      <Input className="input" id="form-email" placeholder="email" />
+      <Input className="input" id="form-phone" placeholder="phone" />
+      <Select className="input" id="form-gender" defaultValue="male">
         <Option value="male">ชาย</Option>
         <Option value="female">หญิง</Option>
-      </Select> <br />
-      <Button type="primary">submit</Button>
+      </Select>{' '}
+      <br />
+      <Button
+        type="primary"
+        onClick={handleSubmit
+          .bind
+          //   {
+          //   name: $('form-name').value,
+          //   email: $('form-email').value,
+          //   phone: $('form-phone').value,
+          //   gender: $('form-gender').value
+          // }
+          ()}
+      >
+        submit
+      </Button>
     </div>
   );
-}
+};
